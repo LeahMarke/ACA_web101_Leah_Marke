5c368183cd1167ed0de2e65e68dc721c5b7b60b3
leahmarke <lmarke@live.com>
2022-05-22 01:23:06 -0400

    working on results page

---
+++ myPortfolio/contact.js
@@ -0,0 +1,7 @@
+// const placeForResults = document.getElementById("results")
+
+// new URLSearchParams(window.location.search).forEach((value, name)) => {
+//   placeForResults.append(`${name} : ${value}`)
+//   placeForResults.append(document.createElement("br"))
+// }
+// Array.from(document.querySelectorAll('#contact_form input')).reduce((acc, input)) => ({ ...acc,[input.id]: input.value }), {});
